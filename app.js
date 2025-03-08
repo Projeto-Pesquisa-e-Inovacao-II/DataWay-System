8ace871b093d1e99c711f1f3ef214b266c28b72b
Gabriel Andrade <g.andrade0222@gmail.com>
2025-03-08 11:24:05 -0300

    add 'contato' to node api (src) folder (#4)
    * fix index error in data-viz
    
    * header & footer components, contact page, image folder
    
    * add 'contato' to node api (src) folder

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ var HOST_APP = process.env.APP_HOST;
 var app = express();
 
 var indexRouter = require("./src/routes/index");
+var contatoRouter = require("./src/routes/contato");
 var usuarioRouter = require("./src/routes/usuarios");
 
 app.use(express.json());
@@ -25,7 +26,11 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 
 app.use("/", indexRouter);
+app.use("/contato", contatoRouter);
 app.use("/usuarios", usuarioRouter);
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.listen(PORTA_APP, function () {
     console.log(`
