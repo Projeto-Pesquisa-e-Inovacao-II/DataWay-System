9adfbf85c936b9ef0420d00e5d2e639eb1c24606
andradeGabriel05 <gabriel.andrade@sptech.school>
2025-04-19 17:51:33 -0300

    v0.0.3 add charts to dashboard with chart.js integration

---
+++ public/js/dashboard/line.js
@@ -0,0 +1,68 @@
+
+
+
+const labels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"];
+
+
+const backgroundPlugin = {
+  id: 'custom_canvas_background_color',
+  beforeDraw: (chart) => {
+    const {ctx, chartArea: {left, top, width, height}} = chart;
+    ctx.save();
+    ctx.fillStyle = 'rgba(65, 183, 213, 0.64)';
+    ctx.fillRect(left, top, width, height);
+    ctx.restore();
+  }
+};
+
+const data = {
+  labels: labels,
+  datasets: [
+    {
+      label: 'My First Dataset',
+      data: [65, 59, 80, 81, 56, 55, 40],
+      fill: true,
+      borderColor: "rgba(108, 230, 232, 0.56)",
+      backgroundColor: "rgba(108, 230, 232, 0.56)",
+      tension: 0.1,
+    },
+    // {
+    //   label: 'Avanços Automáticos',
+    //   data: [10, 30, 50, 65, 60, 80, 90],
+    //   fill: true,
+    //   borderColor: '#ff6384',
+    //   backgroundColor: 'rgba(255, 99, 132, 0.2)',
+    //   tension: 0.3
+  ],
+};
+
+const config = {
+  type: "line",
+  data: data,
+  options: {
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  },
+  plugins: [backgroundPlugin]
+};
+
+const canvas = document.getElementById("lineCanvas");
+if (canvas) {
+  const ctx = canvas.getContext("2d");
+  new Chart(ctx, config);
+} else {
+  console.error("Canvas de área não encontrado!");
+}
+
+
+
+
+
+
+
+
+
+
