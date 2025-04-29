473e4ce41f34e99f6a001c85e9652331c3a3f522
andradeGabriel05 <gabriel.andrade@sptech.school>
2025-04-29 20:17:43 -0300

    0.0.8.1.1 reverting index responsive

--- public/css/responsive/home/home_burger.js
+++
@@ -1,6 +0,0 @@
-var burger = document.getElementById("burgerId").style.display;
-
-if(burger == "none"){
-    document.getElementById("burgerId").style.display = "block";
-    document.getElementById("homeId").style.display = "none";
-}
