162109b2e682969cde2b57548fc2b9b373d2abde
andradeGabriel05 <gabriel.andrade@sptech.school>
2025-05-04 20:27:36 -0300

    v0.1.0.2 aside text change

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -13,60 +13,62 @@ use dataway;
 
 CREATE TABLE Usuario (
     idUsuario INT PRIMARY KEY AUTO_INCREMENT,
-    tipoUsuario ENUM('Empresa', 'Funcionario'),
-    foto VARCHAR(255),
-    email VARCHAR(100) UNIQUE NOT NULL,
-    senha VARCHAR(255) NOT NULL,
-    telefone VARCHAR(20) NOT NULL
+    tipoUsuario ENUM('Admin', 'Empresa', 'Funcionario'),
+    email VARCHAR(100) UNIQUE NULL,
+    senha VARCHAR(255) NULL,
+    telefone VARCHAR(20) NULL
 );
 
 CREATE TABLE Empresa (
     idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
-    CNPJ CHAR(14) UNIQUE NOT NULL,
-    representanteLegal VARCHAR(100) NOT NULL,
-    razaoSocial VARCHAR(150) NOT NULL,
-    nomeFantasia VARCHAR(100) NOT NULL,
-    Usuario_idUsuario INT NOT NULL,
+    CNPJ CHAR(18) UNIQUE NULL,
+    representanteLegal VARCHAR(100) NULL,
+    razaoSocial VARCHAR(150) NULL,
+    nomeFantasia VARCHAR(100) NULL,
+    concessionaria VARCHAR(100),
+    Usuario_idUsuario INT NULL,
     FOREIGN KEY (Usuario_idUsuario) REFERENCES Usuario(idUsuario)
 );
 
 CREATE TABLE Endereco (
     idEndereco INT PRIMARY KEY AUTO_INCREMENT,
-    cep CHAR(8) NOT NULL,
-    cidade VARCHAR(100) NOT NULL,
-    estado CHAR(2) NOT NULL,
-    Empresa_idEmpresa INT NOT NULL,
+    numero VARCHAR(10),
+    bairro VARCHAR(50),
+    cep CHAR(9),
+    cidade VARCHAR(100),
+    estado VARCHAR(50),
+    Empresa_idEmpresa INT,
     FOREIGN KEY (Empresa_idEmpresa) REFERENCES Empresa(idEmpresa)
 );
 
 CREATE TABLE Notificacoes (
     idNotificacoes INT PRIMARY KEY AUTO_INCREMENT,
-    dataHora DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    mensagem TEXT NOT NULL,
-    tipoDestinatario ENUM('Empresa', 'Funcionario'),
-    Usuario_idUsuario INT NOT NULL,
+    dataHora DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+    mensagem TEXT NULL,
+    tipoDestinatario ENUM('Usuario', 'Empresa', 'Funcionario'),
+    Usuario_idUsuario INT NULL,
     FOREIGN KEY (Usuario_idUsuario) REFERENCES Usuario(idUsuario)
 );
 
 CREATE TABLE Funcionario (
     idFuncionario INT PRIMARY KEY AUTO_INCREMENT,
-    Nome VARCHAR(100) NOT NULL,
-    Usuario_idUsuario INT NOT NULL,
-    Empresa_idEmpresa INT NOT NULL,
+    Nome VARCHAR(100) NULL,
+    Usuario_idUsuario INT NULL,
+    Empresa_idEmpresa INT NULL,
     FOREIGN KEY (Usuario_idUsuario) REFERENCES Usuario(idUsuario),
     FOREIGN KEY (Empresa_idEmpresa) REFERENCES Empresa(idEmpresa)
 );
 
 CREATE TABLE DadosPracaPedagio (
     idDadosPracaPedagio INT PRIMARY KEY AUTO_INCREMENT,
-    praca VARCHAR(100) NOT NULL,
-    lote VARCHAR(50) NOT NULL,
-    data DATE NOT NULL,
-    hora INT NOT NULL,
-    valor DECIMAL(10,2) NOT NULL,
-    sentido VARCHAR(100) NOT NULL,
-    tpCampo VARCHAR(50),
-    quantidade INT NOT NULL,
+    praca VARCHAR(100) NULL,
+    lote VARCHAR(50) NULL,
+    data DATE NULL,
+    hora TIME NULL,
+    valor DECIMAL(10,2) NULL,
+    sentido VARCHAR(50),
+    tpCampo INT,
+    quantidade INT NULL,
     Categoria VARCHAR(50),
-    Empresa_idEmpresa INT NOT NULL
+    Empresa_idEmpresa INT NULL
 );
