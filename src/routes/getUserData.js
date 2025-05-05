cb6f49e23d9d69fc7870c16447198bae8c72f2b4
andradeGabriel05 <gabriel.andrade@sptech.school>
2025-05-05 15:11:59 -0300

    v0.1.1.2 user image and name in dashboard; change idEmpresa in getUserData route

--- src/routes/getUserData.js
+++ src/routes/getUserData.js
@@ -5,7 +5,7 @@ var getUserDataController = require("../controllers/getUserDataController");
 
 // Recebendo o email como parâmetro na rota e direcionando para a função getUserData de getUserDataController.js
 router.get("/", function (req, res) {
-    const idEmpresa = 1;
+    const idEmpresa = 2;
     console.log(idEmpresa)
     getUserDataController.getUserData(req, res, idEmpresa);
 });
